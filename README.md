7ce3e9952442f6e856bde93e279929c18e900fdb
JillMeyerWCS <jill.meyer@wuerth-cs.com>
2022-06-13 22:53:23 +0200

    step 2

--- README.md
+++ README.md
@@ -2,15 +2,8 @@
 
 Welcome to the flutter würthshop part one.
 
-## Step 1
+## Step 2
 
-To get started, we will display two items from our future result list.
+Now that we have a base item displayed, let's add the diary identification badge as well.
 
-Use the basic layout widgets like `Column` and `Row` to display two result rows with the following data:
-|Name|Approval Number|
-|---|---|
-|Ehrmann GmbH Oberschönegg im Allgäu|BY 77727|
-|Allgäu Hof Müller - Milchmanufaktur GmbH & Co.KG|BW 08119|
-
-Try to create a view that looks like this:
 ![](./Result.png)
--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,54 @@ class DairyApprovalNumbersPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Dairy approval number app")),
-      body: const Center(child: Text("List of approval numbers")),
+      body: Column(
+        children: const [
+        DairyFactoryDisplay(
+          name: "Ehrmann GmbH Oberschönegg im Allgäu",
+          approvalNumber: "BY 77727",
+        ),
+        DairyFactoryDisplay(
+          name: "Allgäu Hof Müller - Milchmanufaktur GmbH & Co.KG",
+          approvalNumber: "BW 08119",
+        ),
+      ]),
+    );
+  }
+}
+
+class DairyFactoryDisplay extends StatelessWidget {
+  const DairyFactoryDisplay({
+    Key? key,
+    required this.name,
+    required this.approvalNumber,
+  }) : super(key: key);
+
+  final String name;
+  final String approvalNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Icon(Icons.factory_outlined),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(name,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                      fontSize: 20.0, fontWeight: FontWeight.bold)),
+              Text(approvalNumber, style: const TextStyle(fontSize: 14.0))
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
