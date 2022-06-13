bc2022ac51235a82f72f1bfd46e7b1956d3b619e
JillMeyerWCS <jill.meyer@wuerth-cs.com>
2022-06-13 23:46:21 +0200

    step 4

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 
 Welcome to the flutter würthshop part one.
 
-## Step 3
+## Step 4
 
-Let's replace the middle column of the badge with an actual text field.
+Now that we have the base layout ready, let's load some actual data.
+To do this we will have to do the follwing:
 
-![](./Result.png)
+- download the [list as xml](https://apps2.bvl.bund.de/bltu/app/process/bvl-btl_p_veroeffentlichung?execution=e1s6&osForm=osForm&javax.faces.ViewState=e1s6&osForm%3Abvl_002dbtl_002ep__veroeffentlichung_002eunterliste__part_002edGrid%3A50%3Abvl_002dbtl_002ep__veroeffentlichung_002eunterliste__part_002edGrid_002eexport0=clicked)
+- move to `./assets/codes.xml`
+- declare asset in [pubspec](./pubspec.yaml)
+- load asset in code
+- show status of loading the asset in code
+
+![](./Result.gif)
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,34 +27,61 @@ class DairyApprovalNumbersPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Dairy approval number app")),
-      body: Row(
-        children: [
-          const DairyIdentificationBadge(),
-          Expanded(
-            child: Column(children: const [
-              DairyFactoryDisplay(
-                name: "Ehrmann GmbH Oberschönegg im Allgäu",
-                approvalNumber: "BY 77727",
-              ),
-              DairyFactoryDisplay(
-                name: "Allgäu Hof Müller - Milchmanufaktur GmbH & Co.KG",
-                approvalNumber: "BW 08119",
-              ),
-            ]),
-          ),
-        ],
-      ),
+      body: DairySearchForm(),
+    );
+  }
+}
+
+class DairySearchForm extends StatefulWidget {
+  const DairySearchForm({Key? key}) : super(key: key);
+
+  @override
+  State<DairySearchForm> createState() => _DairySearchFormState();
+}
+
+class _DairySearchFormState extends State<DairySearchForm> {
+  final _inputController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        DairyIdentificationBadge(
+          controller: _inputController,
+        ),
+        Expanded(
+          child: Column(children: const [
+            DairyFactoryDisplay(
+              name: "Ehrmann GmbH Oberschönegg im Allgäu",
+              approvalNumber: "BY 77727",
+            ),
+            DairyFactoryDisplay(
+              name: "Allgäu Hof Müller - Milchmanufaktur GmbH & Co.KG",
+              approvalNumber: "BW 08119",
+            ),
+          ]),
+        ),
+      ],
     );
   }
 }
 
 class DairyIdentificationBadge extends StatelessWidget {
+  final TextEditingController controller;
+
   const DairyIdentificationBadge({
     Key? key,
+    required this.controller,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      fontSize: 24,
+    );
     return SizedBox(
         width: 220,
         child: Center(
@@ -64,15 +92,21 @@ class DairyIdentificationBadge extends StatelessWidget {
               border: Border.all(),
               borderRadius: const BorderRadius.all(Radius.elliptical(120, 80))),
           child: DefaultTextStyle.merge(
-            style: const TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w800,
-              fontFamily: 'Roboto',
-              fontSize: 24,
-            ),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [Text("DE"), Text("BY 12312"), Text("EG")]),
+            style: textStyle,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text("DE"),
+              TextField(
+                textAlign: TextAlign.center,
+                style: textStyle,
+                controller: controller,
+                decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    isDense: true,
+                    hintText: "App. Nr."),
+              ),
+              const Text("EG")
+            ]),
           ),
         )));
   }
